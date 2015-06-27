59f2ae93fb39bf47481b1966e27ad9bb98cecf82
rmorina <rudina93@gmail.com>
2015-06-27 16:15:04 -0400

    added getHomePageList

--- OrderController.js
+++ OrderController.js
@@ -3,6 +3,20 @@ var OrderController = {
 	order: function() {
 		console.log("OrderController.order function called");
 	},
+	
+	getHomePageList: function() {
+		var elements = new Array();
+		elements.push($("#sign-in"));
+		elements.push($(".button-big sign-up button-secondary"));
+		elements.push($("#download-launcher"));
+		elements.push($("#try-dfb"));
+		return elements;
+	},
+	
+	getMydropboxList: function() {
+	
+	},
+	
 }; 
 
 	
