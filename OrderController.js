8ddc36c78e840c5aca2d3062a17c4b4491105b80
Kelsey Hrubes <kelseyhrubes@gmail.com>
2015-06-27 11:42:06 -0700

    add OrderController class

---
+++ OrderController.js
@@ -0,0 +1,7 @@
+var OrderController = {	
+	homePage: {
+		welcomeToDropbox: "just an empty object for now, we will specify something telling chromevox to say welcome to dropbox"
+	},
+	myDropBox: {},
+};
+
--- contentscript.js
+++ contentscript.js
@@ -1,2 +1,5 @@
 $(document).attr("title", "WE BALLERS");
 console.log(document.body);
+
+
+console.log(this.OrderController);
