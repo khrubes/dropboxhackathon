3d88d2b0217440e2da567911bfc36e143cbd9d21
Hannah Perry <hannah.perry5@gmail.com>
2015-06-27 15:34:44 -0700

    Making list of focused items

--- ElementHandler.js
+++ ElementHandler.js
@@ -1,9 +1,21 @@
 var ElementHandler = (function (ElementHandler) {
 
     ElementHandler.handleDocument = function() {
-	var all = document.getElementsByTagName("*");
-	for (var i=0, max=all.length; i < max; i++) {
-	    this.handleHTMLElement(all[i]);
+    	var speak_elements = [];
+    	var speak_text = [];
+		var all = document.getElementsByTagName("*");
+		for (var i=0, max=all.length; i < max; i++) {
+	    	this.handleHTMLElement(all[i]);
+	    	switch(all[i].getAttribute("id")) {
+		    	case "sign-in":
+		    		all[i].setAttribute("tabIndex", 0);
+	    			speak_elements[0] = all[i];
+	    			speak_text[0] = "Sign in link";
+	    			break;
+	    }
+	   	for (var i=0, max=speak_elements.length; i < max; i++) {
+	   		// call speak(speak_elements[i], speak_text[i]);
+	   	}
 	}
 	
     }
