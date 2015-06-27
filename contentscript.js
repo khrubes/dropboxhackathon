c51bce4789848ffd07439c45d8b14070451e0a8a
Kelsey Hrubes <kelseyhrubes@gmail.com>
2015-06-27 13:30:11 -0700

    fix merge conflict

--- contentscript.js
+++ contentscript.js
@@ -1,10 +1,8 @@
 $(document).attr("title", "WE BALLERS");
 
-<<<<<<< HEAD
-this.OrderController.order(OrderController.getHomePageList());
-=======
 
-this.OrderController.order();
+this.OrderController.order(OrderController.getHomePageList());
+
 this.ElementHandler.handleDocument();
 //made this no args because document is global var
->>>>>>> ec56a26a209d3b07d8b99dd382e68660b331e774
+
