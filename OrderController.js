acef0ea0f376959d0e4506548ecadde75c33081c
rmorina <rudina93@gmail.com>
2015-06-27 14:49:55 -0400

    OrderController skeleton

--- OrderController.js
+++ OrderController.js
@@ -1,7 +1,22 @@
-var OrderController = {	
-	homePage: {
-		welcomeToDropbox: "just an empty object for now, we will specify something telling chromevox to say welcome to dropbox"
-	},
-	myDropBox: {},
-};
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+var OrderController = (function (OrderController) {
+  
+	OrderController.orderHomepage = function() {
+		Console.log("OrderController.orderHomepage");
+	};
+
+	OrderController.orderMyDropbox = function() {
+		Console.log("OrderController.orderMyDropbox");
+	};
+  
+	return OrderController;
+
+} (OrderController || {}));
+
+
 
