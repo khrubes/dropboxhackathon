a5b49013af48e9e6d873885417ea5bf73957401b
Kelsey Hrubes <kelseyhrubes@gmail.com>
2015-06-27 11:56:43 -0700

    add elementhandler and update ordercontroller

--- ElementHandler.js
+++ ElementHandler.js
@@ -1,3 +1,9 @@
-var ElementHandler = function(){
-	
-}
+var ElementHandler = (function (ElementHandler) {
+  
+	ElementHandler.handleHTMLElement = function(element) {
+		Console.log("ElementHandler.handleHTMLElement"+ element);
+	};
+  
+	return ElementHandler;
+
+} (ElementHandler || {}));
